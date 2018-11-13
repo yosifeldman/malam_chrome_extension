7b9a48bfa34f241e8af498662fc61fbf7edf8546
Yosef Feldman <yoseff@r7devp24.sis.local>
2018-11-13 14:44:58 +0200

    Fixed for new employees

--- inject.js
+++ inject.js
@@ -2,10 +2,11 @@
  * Created by yosefF on 9/5/2018.
  */
 var processInsertHours = function (h) {
-    console.log('eHarmonyJson');
-    console.log(h);
+    //console.log('eHarmonyJson');
+    //console.log(h);
     h = h.eHarmonyJson;
     let y = (new Date).getFullYear(), d, v, p = {}, s, msg, k, c = h[0].length * (h.length - 1);
+    //console.log("C = "+c);
     for (let i = 0; i < h[0].length; i++) {
         for (let j = 1; j < h.length; j++) {
             k = "pt1:dataTable:" + (j - 1);
@@ -16,8 +17,11 @@ var processInsertHours = function (h) {
                     wrongDateAlert("The eHarmony date " + d + " is not present on this report.");
                     return false;
                 }
-                else if (v.value !== d) {
-                    wrongDateAlert("The eHarmony date " + d + " does not match the Malam date " + v.value + ".");
+
+                // scary, but necessary patch - try to handle the case of new employee, when
+                // the eHarmony begins in the middle of the month
+                else if (v.value !== d && !isLaterInTheMonth(d, v.value)) {
+                    wrongDateAlert("The eHarmony date " + d + " is in wrong month/year. This Malam report is for " + v.value.substr(3) + ".");
                     return false;
                 }
                 p[h[j][2]] = 0;
@@ -64,10 +68,11 @@ var detectProblems = function (prob) {
 var wrongDateAlert = function (t) {
     alert("ERROR: " + t +
         "\n\nCANNOT PROCEED - please do:\n" +
-        "   1. download from eHarmony again, the date range report,\n" +
+        "   1. make sure you're trying to upload the report for the right month/year" +
+        "   2. if yes, - download from eHarmony again, the date range report,\n" +
         "       that is between " + getFirstOfMonth() + " and " + getLastOfMonth() + ".\n" +
-        "   2. UNDO all changes on this page (e.g. press OK and reload page).\n" +
-        "   3. try to import again.");
+        "   3. UNDO all changes on this page (e.g. press OK and reload page).\n" +
+        "   4. try to import again.");
 };
 
 var getFirstOfMonth = function () {
@@ -78,4 +83,9 @@ var getLastOfMonth = function () {
     const e = document.getElementById("pt1:dataTable:30:clockInDate::content");
     if (e) return e.value;
     return document.getElementById("pt1:dataTable:29:clockInDate::content").value;
+};
+
+var isLaterInTheMonth = function (issueDate, correctDate) {
+    var s1 = issueDate.substr(2), s2 = correctDate.substr(2), s3 = issueDate.substr(0,2), s4 = correctDate.substr(0,2);
+    return (s1 === s2) && (s3 > s4);
 };
